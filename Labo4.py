236271d363f525dde62a65d2bd66938f63b75503
Toxiko8 <arnaud.clarat@live.fr>
2019-02-27 21:07:09 +0100

    Affichage et calcul total à finir

--- Labo4.py
+++ Labo4.py
@@ -44,7 +44,6 @@ def listeSauts(nbrSaut):
 
 def noteSaut(nbrSaut):
 	global datas
-	j = 0
 	print("\nSaut " + str(nbrSaut))
 	for plongeur in datas:
 		notesSaut = []
@@ -52,25 +51,39 @@ def noteSaut(nbrSaut):
 		for i in range(5):
 			note = float(input("Points jury " + str(i + 1) + " : "))
 			notesSaut.append(note)
-		datas[j][nbrSaut].append(notesSaut)
-		j += 1
+		plongeur[nbrSaut].append(notesSaut)
 
 
 def calculPoints(nbrSaut):
 	global datas
-	j = 0
 	for plongeur in datas:
 		subTot = 0
 		points = triBulle(plongeur[nbrSaut][2])[1:-1]
 		for point in points:
 			subTot += point
-			total = subTot * plongeur[nbrSaut][1]
-		datas[j][nbrSaut].append(total)
-		j += 1
-	print(datas)
+		total = subTot * plongeur[nbrSaut][1]
+		plongeur[nbrSaut].append(total)
 
 
+def cumul():
+	global datas
+	calculPoints(2)
+	for plongeur in datas:
+		point1 = plongeur[1][3]
+		point2 = plongeur[2][3]
+		plongeur.append(point1 + point2)
 
+
+def affichage():
+	data = []
+	for plongeur in datas:
+		temp = []
+		temp.append(plongeur[0])
+		temp.append(plongeur[3])
+		data.append(temp)
+	print(triBulle(data))
+	print("\nClassement final")
+	#for plongeur in datas:
 
 def main():
 	#newPlongeurs()
@@ -78,11 +91,16 @@ def main():
 	#listeSauts(1)
 	#noteSaut(1)
 	#calculPoints(1)
+	#listeSauts(2)
+	#noteSaut(2)
+	cumul()
+	affichage()
+	print(datas)
 
 
 #--Variables--
 plongeurs = []
-datas = [['P', ['p1', 1.0, [8.0, 10.0, 3.0, 8.0, 8.0], 24.0], ['p2', 2.0]], ['M', ['m1', 1.0, [9.0, 9.0, 8.9, 9.0, 9.0], 27.0], ['m2', 2.0]], ['J', ['j1', 1.0, [4.0, 3.0, 7.0, 5.0, 3.0], 12.0], ['j2', 2.0]]]
+datas = [['Durant Pol', ['Plongeon en avant tendu', 1.6, [8.0, 10.0, 3.0, 8.0, 8.0], 24.0], ['Plongeon retourné groupé', 2.0, [5.0, 6.0, 2.0, 10.0, 8.0]]], ['Lariviere Marcel', ['Salto et demi avant avec une vrille', 2.4, [9.0, 9.0, 8.9, 9.0, 9.0], 27.0], ['Salto et demi arrière groupé', 2.0, [9.0, 8.2, 8.2, 9.0, 10.0]]], ['Lebouteu Jules', ['Salto et demi arrière groupé', 2.0, [4.0, 3.0, 7.0, 5.0, 3.0], 12.0], ['Salto et demi arrière groupé', 2.0, [10.0, 9.8, 8.9, 9.0, 7.9]]]]
 
 if __name__ == "__main__":
 	main()
